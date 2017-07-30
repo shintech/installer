90a60eb1eccbcd171b4f036af333c3a54e6a1366
Michael Prather <mprather1@gmail.com>
2017-07-30 07:24:02 +0000

    0.0.3

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
-var fs = require('fs')
-var pify = require('pify')
+const fs = require('fs')
+const pify = require('pify')
 const yaml = require('js-yaml')
-var execa = require('execa')
-var path = require('path')
-var chalk = require('chalk')
-var currentDirectory = process.argv[2]
-var pathDir = path.join(process.argv[3], 'opt', 'bin')
-var config = path.join(currentDirectory, 'config.yml')
-var command = `#!/usr/bin/env bash\n /usr/local/bin/node ${currentDirectory}/index.js $(pwd) $HOME\n`
+const execa = require('execa')
+const path = require('path')
+const chalk = require('chalk')
+const currentDirectory = process.argv[2]
+const pathDir = path.join(process.env['HOME'], 'opt', 'bin')
+const config = path.join(currentDirectory, 'version.yml')
+const pkg = path.join(currentDirectory, 'package.json')
 
-fs.stat(config, (err, res) => {
-  if (err) {
-    console.log('config.yml not found...\naborting...')
-    return
-  }
+fs.stat(pkg, (err, res) => {
+  if (err && err.path === pkg) {
+    console.log('package.json not found...\nchecking for config.yml...\n')
+    checkForYaml()
+  } else if (!err) {
+    const _package = require(pkg)
 
-  try {
-    var v = yaml.safeLoad(fs.readFileSync(config, 'utf8'))
-    if (v.app !== undefined) {
-      var appName = v.app
-      var filename = path.join(pathDir, appName)
+    let command = `#!/usr/bin/env bash\n\nHOME=$HOME /usr/local/bin/node ${currentDirectory}/${_package.main} $(pwd) $1\n`
 
-      writeToFile(filename, command, v)
-    } else {
-      throw new Error('config.app is undefined...')
-    }
-  } catch (err) {
-    throw new Error(err)
+    console.log('package.json was found...\ninstalling...\n')
+    writeToFile(_package.name, command, _package)
+  } else {
+    console.log(err)
   }
 })
 
+function checkForYaml () {
+  try {
+    const v = yaml.safeLoad(fs.readFileSync(config, 'utf8'))
+
+    if (v.name !== undefined) {
+      console.log('config.yml was found...\ninstalling...\n')
+
+      let command = `#!/usr/bin/env bash\n\nHOME=$HOME /usr/local/bin/node ${currentDirectory}/${v.main} $(pwd) $1\n`
+
+      writeToFile(v.name, command, v)
+    } else {
+      throw new Error('config.name is undefined...')
+    }
+  } catch (err) {
+    if (err && err.path === config) {
+      console.log('yaml not found...\naborting...\n')
+    } else {
+      console.log(err)
+    }
+  }
+}
+
 function writeToFile (filename, command, v) {
-  pify(fs).writeFile(filename, command)
+  pify(fs).writeFile(path.join(pathDir, filename), command)
   .then(function () {
     chmod(filename, v)
   })
 }
 
 function chmod (file, v) {
-  execa.shell(`chmod +x ${path.join(pathDir, v.app)}`)
+  execa.shell(`chmod +x ${path.join(pathDir, v.name)}`)
   .then(() => {
-    console.log(`successfully installed => ${chalk.green(v.app)}`)
+    console.log(`successfully installed => ${chalk.green(v.name)}`)
   })
 }
