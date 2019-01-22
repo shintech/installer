d359e46a2445d09bff5df3d6d953317c0ad5eb29
Michael Prather <mprather1@gmail.com>
2019-01-22 07:58:15 +0000

    way the hell more complicated than necessary

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const os = require('os')
 const path = require('path')
 const winston = require('winston')
 const { promisify } = require('util')
+const checkIfPackageExists = require('./lib/checkIfPackageExists')
+const getFilenameAndExtension = require('./lib/getFilenameAndExtension')
+const getCommand = require('./lib/getCommand')
+const writeFileToPath = require('./lib/writeFileToPath')
 
 const exists = promisify(fs.stat)
 const writeFile = promisify(fs.writeFile)
@@ -11,7 +15,6 @@ const chmod = promisify(fs.chmod)
 const FILEPATH = process.argv[2]
 const HOME = os.homedir()
 const PATH = path.join(HOME, 'opt', 'bin')
-const PWD = process.cwd()
 
 const logger = winston.createLogger({
   transports: [
@@ -27,63 +30,22 @@ const logger = winston.createLogger({
 main()
 
 async function main () {
-  let pkgExists
+  if (!FILEPATH) { throw new Error('No file path specified...') }
+
+  const pkgExists = await checkIfPackageExists(exists(path.join(FILEPATH, 'package.json')))
+
+  pkgExists ? logger.info('package.json was found...') : logger.info('package.json was not found...')
+
+  let file = getFilenameAndExtension(pkgExists, FILEPATH)
+  let command = getCommand(file, FILEPATH)
 
   try {
-    await exists(path.join(FILEPATH || PWD, 'package.json'))
-    pkgExists = true
+    logger.info(`Adding ${FILEPATH} to path...`)
+
+    let write = await writeFileToPath(file, command, PATH, writeFile, chmod)
+
+    logger.info(`${write}...`)
   } catch (err) {
-    pkgExists = false
+    logger.error(`${err}...`)
   }
-
-  let ext, newPath, filename
-
-  if (FILEPATH !== undefined && !pkgExists) {
-    newPath = path.join(PWD, FILEPATH)
-    ext = path.extname(FILEPATH)
-
-    let split = newPath.split('/')
-
-    filename = path.parse(split[split.length - 1]).name
-  } else if (pkgExists) {
-    const pkg = require(path.join(PWD, 'package.json'))
-
-    newPath = PWD
-    ext = '.js'
-    filename = pkg.name
-  } else {
-    throw new Error('No filepath specified and no package.json found')
-  }
-
-  let command = getCommand(filename, newPath, ext)
-
-  try {
-    let file = path.join(PATH, filename)
-
-    logger.info(`Adding ${file} to PATH...`)
-
-    await writeFile(file, command)
-    await chmod(file, '0755')
-  } catch (err) {
-    throw new Error('Error writing file')
-  }
-}
-
-function getCommand (name, filepath, extension) {
-  if (!extension) { 
-    logger.error(name, filepath, extension)
-    throw new Error('File extension not recognized')
-  }
-
-  let command
-
-  if (extension === '.sh') {
-    command = `/usr/bin/env bash ${filepath} "\${1}" "\${2}" "\${3}"`
-  }
-
-  if (extension === '.js') {
-    command = `HOME=$HOME /usr/local/bin/node ${filepath} $(pwd) "\${1}"`
-  }
-
-  return `#!/usr/bin/env bash\n\n#${name}\n\n${command}\n`
 }
